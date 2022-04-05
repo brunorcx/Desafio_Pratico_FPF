a2258c0752cbcd6e7bf94744b4296ad354ec5de2
Bruno RC <brunorcx@hotmail.com>
2022-04-05 16:20:04 -0400

    initial layout for home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,22 @@ import React, { useState } from "react";
 import "styles/home.css";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [openSideMenu, setOpenSideMenu] = useState(false);
   return (
-    <div>
+    <div className="total-home">
       <Navbar openSideMenu={openSideMenu} setOpenSideMenu={setOpenSideMenu} />
-      Aqui está o home!!!!
+      <main className="home-container">
+        <img src="home.jpg" alt="imagem de armadura de cavaleiro" title="Photo by Nik Shuliahin on Unsplash"></img>
+        <h1>Bem-vindo ao Jogo Batalha Única</h1>
+      </main>
+      <div>
+        <h2>Leia as regras antes de jogar, você pode acessá-las no topo da página</h2>
+        <p>Já leu? Então o que está esperando?</p>
+        <Link to="/Game">Jogue agora!</Link>
+      </div>
       <div className={openSideMenu ? "openSideMenu" : "sideMenu"}></div>
       <Footer />
     </div>
