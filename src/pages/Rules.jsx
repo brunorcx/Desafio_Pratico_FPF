b30a931001e244988a34f0eea23bc891a8595b27
Bruno RC <brunorcx@hotmail.com>
2022-04-05 13:59:45 -0400

    Rules for PC done; added placeholder footer

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -22,6 +22,41 @@ const Rules = () => {
             </p>
           </div>
         </div>
+        <p>O jogador deverá ter 4 botões de interação a seguir:</p>
+        <div className="actions-4-container">
+          <div className="card-1">
+            <h2>Ataque Básico</h2>
+            <p>
+              O jogador irá causar um dano no inimigo que será reduzido dos pontos de vida do mesmo, deve ser um
+              valor aleatório entre 5 e 10.
+            </p>
+          </div>
+          <div className="card-2">
+            <h2>Ataque Especial</h2>
+            <p>
+              O jogador irá causar um dano no inimigo que será reduzido dos pontos de vida do mesmo, deve ser um
+              valor aleatório entre 10 e 20.
+            </p>
+            <p>
+              O ataque especial necessita de no mínimo 2 turnos após utilização e deverá estar desabilitado
+              enquanto não carregado, ou seja, o botão não pode ser acionado por 2 turnos após sua utilização.
+            </p>
+            <p>
+              Ao acionar o ataque especial o inimigo tem 50% de chance de não atacar no próximo turno, e deve ser
+              apresentado um indicativo de que o inimigo está atordoado.
+            </p>
+          </div>
+          <div className="card-3">
+            <h2>Curar</h2>
+            <p>
+              O jogador receberá um aumento nos seus pontos de vida que deverá ser um valor randômico entre 5 e 15.
+            </p>
+          </div>
+          <div className="card-4">
+            <h2>Desistir</h2>
+            <p>O jogo deverá ser terminado e ir para tela de inicio do jogo</p>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
