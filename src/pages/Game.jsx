849439e5d1d3621a79e1328eb8e10c108a720627
Bruno RC <brunorcx@hotmail.com>
2022-04-05 18:25:19 -0400

    enemy section with life bar working

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "styles/game.css";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 
 const Game = () => {
+  const enemyLifebarEl = useRef(null);
   return (
     <div>
       <Navbar />
-      Game page
+      <main className="game-container">
+        <div className="enemy">
+          <div className="lifebar-container">
+            <div className="lifebar-progress" ref={enemyLifebarEl}>
+              100%
+            </div>
+          </div>
+          <img
+            src="sprites/dorver/dorver.gif"
+            alt="monstro de quatro patas inimigo"
+            title="Manveru, Dorimen at https://opengameart.org/content/dorver-monster"
+          ></img>
+          <button
+            onClick={() => {
+              enemyLifebarEl.current.style.width = "50%";
+            }}
+          ></button>
+        </div>
+        <div className="player"></div>
+        <div className="game-log"></div>
+      </main>
       <Footer />
     </div>
   );
