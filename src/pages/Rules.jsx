fe5b3fbaaa9dc22725f9ddb71c35dadca0d400da
Bruno RC <brunorcx@hotmail.com>
2022-04-05 10:46:52 -0400

    rules page layout initial config

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "styles/game.css";
+import "styles/rules.css";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 
@@ -7,7 +7,22 @@ const Rules = () => {
   return (
     <div>
       <Navbar />
-      Rules page
+      <main className="rules-container">
+        <h1>Regras do Jogo</h1>
+        <p>Preste atenção nas regras para conseguir uma boa pontuação!</p>
+        <div className="card-container">
+          <div className="center-card">
+            <p>
+              O jogo deverá ser de turnos, sendo cada turno uma interação do jogador e uma interação automática do
+              inimigo.
+            </p>
+            <p>
+              O jogador e o inimigo deverão ter uma barra de vida, ambos terão a mesma quantidade de pontos de vida
+              (100).
+            </p>
+          </div>
+        </div>
+      </main>
       <Footer />
     </div>
   );
