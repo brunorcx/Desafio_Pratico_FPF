650dc676cabe419fad1017d4c2c50fa7d1145628
Bruno RC <brunorcx@hotmail.com>
2022-04-05 20:14:36 -0400

    updated rules colors; added player section with health bar and commands

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -5,6 +5,7 @@ import Footer from "components/Footer";
 
 const Game = () => {
   const enemyLifebarEl = useRef(null);
+  const playerLifebarEl = useRef(null);
   return (
     <div>
       <Navbar />
@@ -26,7 +27,22 @@ const Game = () => {
             }}
           ></button>
         </div>
-        <div className="player"></div>
+        <div className="player">
+          <div className="player-info">
+            <div className="lifebar-container">
+              <div className="lifebar-progress" ref={playerLifebarEl}>
+                100%
+              </div>
+            </div>
+            <img src="sprites/knight/knight2.gif" alt="jogador"></img>
+          </div>
+          <div className="player-controls-container">
+            <button className="attack">Atacar</button>
+            <button className="special-attack">Ataque Especial</button>
+            <button className="heal">Curar</button>
+            <button className="give-up">Desistir</button>
+          </div>
+        </div>
         <div className="game-log"></div>
       </main>
       <Footer />
