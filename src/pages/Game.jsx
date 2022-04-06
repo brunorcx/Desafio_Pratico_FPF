8d0d5d4c134c4b69d08c45e1faa954adcedbf751
Bruno RC <brunorcx@hotmail.com>
2022-04-06 09:11:49 -0400

    player attack logic working

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "styles/game.css";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
@@ -6,6 +6,16 @@ import Footer from "components/Footer";
 const Game = () => {
   const enemyLifebarEl = useRef(null);
   const playerLifebarEl = useRef(null);
+  const [enemyLife, setEnemyLife] = useState(100);
+  const [playerLife, setPlayerLife] = useState(100);
+
+  function playerAttack(enemyLife) {
+    setEnemyLife(enemyLife - Math.floor(Math.random() * (10 - 5 + 1) + 5));
+  }
+  useEffect(() => {
+    enemyLifebarEl.current.style.width = enemyLife + "%";
+  }, [enemyLife]);
+
   return (
     <div>
       <Navbar />
@@ -13,7 +23,7 @@ const Game = () => {
         <div className="enemy">
           <div className="lifebar-container">
             <div className="lifebar-progress" ref={enemyLifebarEl}>
-              100%
+              {enemyLife}%
             </div>
           </div>
           <img
@@ -31,13 +41,15 @@ const Game = () => {
           <div className="player-info">
             <div className="lifebar-container">
               <div className="lifebar-progress" ref={playerLifebarEl}>
-                100%
+                {playerLife}%
               </div>
             </div>
             <img src="sprites/knight/knight2.gif" alt="jogador"></img>
           </div>
           <div className="player-controls-container">
-            <button className="attack">Atacar</button>
+            <button className="attack" onClick={() => playerAttack(enemyLife)}>
+              Atacar
+            </button>
             <button className="special-attack">Ataque Especial</button>
             <button className="heal">Curar</button>
             <button className="give-up">Desistir</button>
