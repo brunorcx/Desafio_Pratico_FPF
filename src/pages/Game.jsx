d11933bf0212749e80e0a0dcc18a10e731668e58
Bruno RC <brunorcx@hotmail.com>
2022-04-06 13:15:32 -0400

    Enemy attacks after player takes action

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -6,24 +6,37 @@ import Footer from "components/Footer";
 const Game = () => {
   const enemyLifebarEl = useRef(null);
   const playerLifebarEl = useRef(null);
+  const turns = useRef(0);
   const [enemyLife, setEnemyLife] = useState(100);
   const [playerLife, setPlayerLife] = useState(100);
 
+  //Player actions
   function playerAttack(enemyLife) {
     setEnemyLife(enemyLife - Math.floor(Math.random() * (10 - 5 + 1) + 5));
+    turns.current++;
   }
   function playerSpecialAttack(enemyLife) {
     setEnemyLife(enemyLife - Math.floor(Math.random() * (20 - 10 + 1) + 10));
+    turns.current++;
   }
   function healPlayer(playerLife) {
     setPlayerLife(playerLife + Math.floor(Math.random() * (15 - 5 + 1) + 5));
+    turns.current++;
+  }
+  //Enemy actions
+  function enemyAttack(playerLife) {
+    setPlayerLife(playerLife - Math.floor(Math.random() * (12 - 6 + 1) + 6));
+  }
+  function enemySpecialAttack(playerLife) {
+    setPlayerLife(playerLife - Math.floor(Math.random() * (16 - 8 + 1) + 8));
   }
 
+  //Player actions
   useEffect(() => {
     if (enemyLife < 0) {
       enemyLifebarEl.current.style.width = "0%";
       setEnemyLife(0);
-    } else {
+    } else if (enemyLife !== 100) {
       enemyLifebarEl.current.style.width = enemyLife + "%";
     }
   }, [enemyLife]);
@@ -39,6 +52,19 @@ const Game = () => {
       playerLifebarEl.current.style.width = playerLife + "%";
     }
   }, [playerLife]);
+
+  //Enemy actions
+  useEffect(() => {
+    //wait one second before enemy attacks
+    //skip first render
+    if (turns.current > 0) {
+      setTimeout(() => {
+        if (playerLife > 0) {
+          enemyAttack(playerLife);
+        }
+      }, 1000);
+    }
+  }, [enemyLife, turns.current]);
 
   return (
     <div>
