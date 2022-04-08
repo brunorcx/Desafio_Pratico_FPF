fcaba9782fad5147d61fd022fefd32d7982cc612
Bruno RC <brunorcx@hotmail.com>
2022-04-08 09:59:11 -0400

    toggle sideMenu working for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,16 @@ const Home = () => {
   return (
     <div className="total-home">
       <Navbar openSideMenu={openSideMenu} setOpenSideMenu={setOpenSideMenu} />
+      <div className={openSideMenu ? "openSideMenu" : "sideMenu"}>AAAAAAAAAAAAA</div>
       <main className="home-container">
         <img src="home.jpg" alt="imagem de armadura de cavaleiro" title="Photo by Nik Shuliahin on Unsplash"></img>
         <h1>Bem-vindo ao Jogo Batalha Única</h1>
       </main>
-      <div>
+      <div className="intro">
         <h2>Leia as regras antes de jogar, você pode acessá-las no topo da página</h2>
         <p>Já leu? Então o que está esperando?</p>
         <Link to="/Game">Jogue agora!</Link>
       </div>
-      <div className={openSideMenu ? "openSideMenu" : "sideMenu"}></div>
       <Footer />
     </div>
   );
